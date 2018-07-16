f0ffeeec580963642b71e0d1083520fbc26f14cb
xonix <xonixx@gmail.com>
2018-07-16 14:45:37 +0300

    something basic

---
+++ src/components/Root.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="root">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Root',
+  data () {
+    return {
+      msg: 'Streaming market data'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
