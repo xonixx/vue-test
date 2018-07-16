7ef1589719c770f761d7b285d1a4b31c4df3aa3e
xonix <xonixx@gmail.com>
2018-07-16 15:21:01 +0300

    connect to websocket

--- src/components/Root.vue
+++ src/components/Root.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="root">
-    {{ msg }}
+    {{ price }}
   </div>
 </template>
 
 <script>
+import { WebSocketSubject } from 'rxjs/webSocket'
+
 export default {
   name: 'Root',
+  created () {
+    const ws = new WebSocketSubject('ws://159.89.15.214:8080')
+    ws.subscribe(
+      (data) => {
+        this.price = data.price
+      },
+      (err) => console.error(err),
+      () => console.warn('Completed!')
+    )
+    ws.next({'subscribe': 'DE000BASF111'})
+    console.log('here')
+  },
   data () {
     return {
-      msg: 'Streaming market data'
+      msg: 'Streaming market data',
+      price: ''
     }
   }
 }
