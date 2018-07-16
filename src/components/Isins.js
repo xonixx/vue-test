a16bf3de9a94fae2e08e051d5426f975591618fd
xonix <xonixx@gmail.com>
2018-07-16 16:18:16 +0300

    ISINs

---
+++ src/components/Isins.js
@@ -0,0 +1,12 @@
+export default [
+  {name: 'Microsoft Corp.', isin: 'US5949181045'},
+  {name: 'Google Inc.', isin: 'US38259P5089'},
+  {name: 'Apple Inc.', isin: 'US0378331005'},
+  {name: 'Bear Zertifikat auf DAX', isin: 'NL0000729408'},
+  {name: 'Yusen Logistics Co.', isin: 'JP3946600008'},
+  {name: 'Aktienanleihe auf BASF', isin: 'DE000DZ21632'},
+  {name: 'Aktienanleihe Plus auf Al', isin: 'DE000DB7HWY7'},
+  {name: 'Aktienanleihe Plus auf', isin: 'DE000CM7VX13'},
+  {name: 'BMW Australia', isin: 'CH0031240127'},
+  {name: 'Yorbeau Res Inc.', isin: 'CA9861913023'}
+]
--- src/components/Root.vue
+++ src/components/Root.vue
@@ -1,30 +1,50 @@
 <template>
   <div class="root">
+    <div class="controls">
+      <select v-model="isin">
+        <option v-for="r in ISINs"
+                :key="r.isin"
+                value="r.isin">
+          {{ r.name }} - {{ r.isin }}
+        </option>
+      </select>
+      <input type="text" v-model="isinText"/>
+    </div>
     {{ price }}
   </div>
 </template>
 
 <script>
 import { WebSocketSubject } from 'rxjs/webSocket'
+import ISINs from './Isins'
 
 export default {
   name: 'Root',
   created () {
-    const ws = new WebSocketSubject('ws://159.89.15.214:8080')
-    ws.subscribe(
-      (data) => {
-        this.price = data.price
-      },
-      (err) => console.error(err),
-      () => console.warn('Completed!')
-    )
-    ws.next({'subscribe': 'DE000BASF111'})
-    console.log('here')
+    this.ISINs = ISINs
+    this.connectWs()
+  },
+  methods: {
+    connectWs () {
+      this.ws = new WebSocketSubject('ws://159.89.15.214:8080')
+      this.ws.subscribe(
+        (data) => {
+          console.info(data)
+          this.price = data.price
+        },
+        (err) => console.error(err),
+        () => console.warn('Completed!')
+      )
+      // ws.next({'subscribe': 'DE000BASF111'})
+      // ws.next({'subscribe': 'AAA'})
+    }
   },
   data () {
     return {
-      msg: 'Streaming market data',
-      price: ''
+      price: '',
+      isin: '',
+      isinText: '',
+      ISINs: []
     }
   }
 }
